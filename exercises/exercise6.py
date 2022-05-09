62a4b18feccbe5972a9b112b056cdc9b0df4aa76
CostasBrian <brian.costas.2014@gmail.com>
2022-05-09 19:13:20 -0300

    resuelto hasta el 6

--- exercises/exercise6.py
+++ exercises/exercise6.py
@@ -1,17 +1,25 @@
 """Type, Comprensión de Listas, Sorted y Filter."""
+print("------------------Aca empieza el codigo--------------------------------")
 
 from typing import List, Union
-
-
-def numeros_al_final_basico(lista: List[Union[float, str]]) -> List[Union[float, str]]:  # noqa: E501
-    """Toma una lista de enteros y strings y devuelve una lista con todos los
+"""Toma una lista de enteros y strings y devuelve una lista con todos los
     elementos numéricos al final.
 
     Restricciones:
         - Utilizar un bucle FOR.
         - Utilizar la función type.
         - No utilizar índices.
-    """
+"""
+
+def numeros_al_final_basico(lista: List[Union[float, str]]) -> List[Union[float, str]]:  # noqa: E501
+    listafinal=[]
+    for elemento in lista:
+       if type(elemento) == str:
+           listafinal.append(elemento)
+    for elemento in lista:
+        if type(elemento) == int:
+           listafinal.append(elemento)
+    return listafinal
 
 
 # NO MODIFICAR - INICIO
@@ -20,17 +28,21 @@ assert numeros_al_final_basico([3, "a", 1, "b", 10, "j"]) == ["a", "b", "j", 3, 
 
 
 ###############################################################################
-
-
-def numeros_al_final_comprension(lista: List[Union[float, str]]) -> List[Union[float, str]]:  # noqa: E501
-    """Re-escribir utilizando comprensión de listas.
+"""Re-escribir utilizando comprensión de listas.
 
     Restricciones:
         - No utilizar bucles.
         - Utilizar dos comprensiones de listas.
-    """
+"""
 
+def numeros_al_final_comprension(lista: List[Union[float, str]]) -> List[Union[float, str]]:  # noqa: E501
+    
+    listafinal_1= [elemento for elemento in lista if type(elemento)== str]
+    listafinal_2= [elemento for elemento in lista if type(elemento)== int]
+    listafinal_1.extend(listafinal_2)
+    return listafinal_1
 
 # NO MODIFICAR - INICIO
 assert numeros_al_final_comprension([3, "a", 1, "b", 10, "j"]) == ["a", "b", "j", 3, 1, 10]  # noqa: E501
 # NO MODIFICAR - FIN
+# Con comprensión de listas
