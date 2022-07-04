ad710aa922b8ad6d41c586aa7ebbe395d57ea21f
CostasBrian <brian.costas.2014@gmail.com>
2022-07-04 18:50:20 -0300

    resuelto el any

--- exercises/exercise7.py
+++ exercises/exercise7.py
@@ -59,16 +59,8 @@ assert not superposicion_in(test_list, (2, "world", 30.85))
 
     Referencia: https://docs.python.org/3/library/functions.html#any
 """
-"""
 def superposicion_any(lista_1: Iterable[Any], lista_2: Iterable[Any]) -> bool:
-   if any(lista_1) == any(lista_2):
-       resultado = True
-       print(resultado)
-       return resultado
-   else:
-       resultado = False
-       print(resultado)
-       return resultado   
+   return any([x == y for x in lista_1 for y in lista_2])
     
 
 
@@ -77,8 +69,6 @@ test_list = [1, "hello", 35.20]
 assert superposicion_any(test_list, (2, "world", 35.20))
 assert not superposicion_any(test_list, (2, "world", 30.85))
 # NO MODIFICAR - FIN
-"""
-
 ###############################################################################
 
 
